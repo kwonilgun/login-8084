47f0fc5897bbcf941f88bb8d0d5921c9100a061d
kwonilgun <kwonilgun@naver.com>
2022-08-06 20:21:46 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,29 +1,21 @@
 "use strict";
-//const fetch = require("node-fetch");
-{/* <body>
-  <h2>A</h2>
-  <script src="./script.js"></script>
-  <h2>B</h2>
-</body>
-위 HTML 문서는 브라우저에서 다음과 같이 순차적으로 처리가 됩니다.
-
-<h2>A</h2>가 화면에 출력됨
-script.js 파일을 내려받아 자바스립트 코드가 실행됨
-<h2>B</h2>가 화면에 출력 */}
-
 
 //DOM -> Document object model 인터페이스 HTML에 존재하는 데이타를 가져와서 제어한다. 
-console.log("hellll")
+console.log("register.js")
 const id = document.querySelector("#id"),
+name = document.querySelector("#name"),
 pswd = document.querySelector("#pswd"),
-loginBtn = document.querySelector("#button");
+confirmPswd = document.querySelector("#confirm-pswd"),
+registerBtn = document.querySelector("#button");
 
-loginBtn.addEventListener("click", login)
+registerBtn.addEventListener("click", register)
 
-function login() {
+function register() {
     const req = {
         id: id.value,
+        name: name.value,
         pswd: pswd.value,
+        confirmPswd: confirmPswd.value,
     };
     console.log(req);
     console.log(JSON.stringify(req));
@@ -32,7 +24,7 @@ function login() {
 
     //console.log(JSON.stringify(body));
 
-    fetch("/login", {
+    fetch("/register", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -41,16 +33,14 @@ function login() {
     }).then((res) => res.json())
     .then((res) => {
         if(res.success) {
-            location.href = "/";
+            location.href = "/login";
         } else {
             console.log(res.msg)
             alert(res.msg);
         }
     })
     .catch((err) => {
-        console.error(new Error("로그인중 에러 발생"));
+        console.error(new Error("회원가입중 에러 발생"));
     })
     ;
 }
-
-//console.log(id);
